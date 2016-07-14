8c587c53e412e86a68233d82622180f7f69236a9
Avidor Turkewitz <avi.turkewitz@flatironschool.com>
2016-07-14 02:20:29 +0000

    Done.

--- shopping.js
+++ shopping.js
@@ -1,3 +1,5 @@
+var cart = []
+
 function total() {
   let t = 0
 
@@ -8,4 +10,32 @@ function total() {
   }
 
   return t
+}
+
+function getCart() {
+  return cart;
+}
+
+function setCart(new_cart) {
+  cart = new_cart;
+}
+
+function randomPrice() {
+  return Math.floor((Math.random()*100));
+}
+
+function addToCart(itemName) {
+  cart.push({[itemName]: randomPrice()})
+  console.log(`${itemName} has been added to your cart.`);
+  return cart;
+}
+
+function viewCart(){
+  if (cart.length == 0){
+    return "Your shopping cart is empty."
+  }else{
+    for (var itemName in cart) {
+      console.log(`In your cart you have ${itemName} ${cart[itemName]}`)
+    }
+  }
 }
